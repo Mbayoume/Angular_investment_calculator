2cf509363f43d69026c5d43c525f252a614207e1
bayoume <mohamed.bayoumi@raneen.com>
2025-05-18 22:33:47 +0300

    Handling the form submission and binding the form properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import { UserInputsComponent } from "./user-inputs/user-inputs.component";
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputsComponent]
+  imports: [HeaderComponent, UserInputsComponent,FormsModule]
 })
 export class AppComponent {}
--- src/app/user-inputs/user-inputs.component.ts
+++ src/app/user-inputs/user-inputs.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,Output,EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-inputs',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-inputs.component.html',
   styleUrl: './user-inputs.component.css'
 })
+
+
 export class UserInputsComponent {
+
+initialInvestmentInput = '0';
+annualInvestmentInput = '0';
+expectedReturnInput = '0';
+durationInput = '0';
+
+
+@Output() submitData = new EventEmitter<string>()
+
+
+
+onSubmitForm(){
+  console.log(this.annualInvestmentInput);
+  console.log(this.expectedReturnInput);
+  console.log(this.annualInvestmentInput);
+  console.log(this.durationInput)
+}
 
 }
