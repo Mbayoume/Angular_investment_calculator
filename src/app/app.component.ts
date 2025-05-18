c989c89ddb6c520ab4c06ca564549a4de057de06
bayoume <mohamed.bayoumi@raneen.com>
2025-05-18 03:22:55 +0300

    CREATE:create  the header and user-inputs components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "./header/header.component";
+import { UserInputsComponent } from "./user-inputs/user-inputs.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [HeaderComponent, UserInputsComponent]
 })
 export class AppComponent {}
---
+++ src/app/user-inputs/user-inputs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-inputs',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-inputs.component.html',
+  styleUrl: './user-inputs.component.css'
+})
+export class UserInputsComponent {
+
+}
